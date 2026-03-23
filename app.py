c9ed19b064d8d55d840b1b473db6d6bc1fa4c3d2
Hemant Raghav <hr246740@gmail.com>
2026-03-23 22:17:09 +0530

    Added CI/CD pipeline with GitHub Actions

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "status": "running",
+        "message": "DevSecOps Pipeline - Hemant Raghav",
+        "version": "2.0",
+        "time": str(datetime.datetime.now())
+    })
+
+@app.route('/health')
+def health():
+    return jsonify({"health": "OK"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
